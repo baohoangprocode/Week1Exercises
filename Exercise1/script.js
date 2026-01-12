a806911e04b2d8a6611faaa7986715639056034d
H82M19\Student <Student>
2026-01-12 08:04:46 +0700

    first commit

---
+++ Exercise1/script.js
@@ -0,0 +1,13 @@
+const marksweight = 78
+const marksheight = 1.69
+const johnweight = 92
+const johnheight = 1.95
+
+const markBMI = marksweight/(marksheight*marksheight)
+const johnBMI = johnweight/(johnheight*johnheight)
+
+const comparison = markBMI > johnBMI
+
+console.log("BMI của Mark:", markBMI);
+console.log("BMI của John:", johnBMI);
+console.log("Mark có BMI cao hơn John không?", comparison);
---
+++ Exercise2/script.js
@@ -0,0 +1,19 @@
+const marksweight = 78
+const marksheight = 1.69
+const johnweight = 92
+const johnheight = 1.95
+
+const markBMI = marksweight/(marksheight*marksheight)
+const johnBMI = johnweight/(johnheight*johnheight)
+
+const comparison = markBMI > johnBMI
+
+console.log("BMI của Mark:", markBMI);
+console.log("BMI của John:", johnBMI);
+console.log("Mark có BMI cao hơn John không?", comparison);
+
+if (comparison) {
+    console.log("Mark's BMI is higher than John's!")
+} else {
+    console.log("John's BMI is higher than Mark's!")
+}

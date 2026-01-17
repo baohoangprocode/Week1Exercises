2e75a6a4d2200506565aff0670b2b554fd07f985
BaoHoang <atmda2468@gmail.com>
2026-01-17 22:47:23 +0700

    Updated

--- React/src/Components/ProductCard/ProductCard.jsx
+++ React/src/Components/ProductCard/ProductCard.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./ProductCard.css";
 import ProductImg from "../../assets/Image/Image72.png";
+import Button from "../Button/Button";
 
-const ProductCard = () => {
+const ProductCard = ({name, price, buttonType}) => {
   return (
     <div className="pc-box">
       <div className="pc-img-box">
         <img className="pc-img" src={ProductImg} alt="Product" />
       </div>
-      <h1 className="pc-name">Spaghetti</h1>
-      <h3 className="pc-price">$35.3</h3>
-      <button className="common-btn">Add to Cart</button>
+      <h1 className="pc-name">{name}</h1>
+      <h3 className="pc-price">{price}</h3>
+      <Button type={buttonType} />
     </div>
   );
 };
